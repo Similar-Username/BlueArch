9fdb458c67073e6c4e41c32471048ff36276d57c
Similar_Username <mannyjrmabalot@gmail.com>
2026-03-26 15:35:07 +0800

    Update install.sh
    Updated install_pkg to support aur packages

--- install.sh
+++ install.sh
@@ -2,13 +2,48 @@
 set -e  
 set -u  
 
-install_pkg() {
-    for pkg in "$@"; do
-        echo "Installing $pkg..."
-        sudo pacman -S --needed --noconfirm "$pkg"
-    done
+install_pacman() {
+    local pkg="$1"
+
+    if pacman -Q "$pkg" &> /dev/null || command -v "$pkg" &> /dev/null; then
+        echo "$pkg already installed"
+        return 0
+    fi
+
+    echo "Installing $pkg..."
+    sudo pacman -S --needed --noconfirm "$pkg"
 }
 
+
+
+install_aur() {
+    local package="$1"
+    local tmpdir="/tmp/$package"
+    
+    if ! command -v "$package" &> /dev/null; then
+        echo " Installing $package from AUR..."
+
+        git clone "https://aur.archlinux.org/$package.git" "$tmpdir"
+        cd "$tmpdir" || { echo "Failed to enter $tmpdir"; return 1; }
+        makepkg -si --noconfirm
+
+        cd - || exit
+        rm -rf "$tmpdir"
+    else
+        echo "$package is already installed."
+    fi
+}
+
+
+install_pkg() {
+    for pkg in "$@"; do
+        if ! install_pacman "$pkg"; then
+            if ! install_aur "$pkg"; then
+                echo "Failed to install $pkg."
+            fi
+        fi
+    done
+}
 
 cd "$(dirname "$0")"
 
