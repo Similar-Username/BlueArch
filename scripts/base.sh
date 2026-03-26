9d03f61b53ef0d870ce4847db40a51dcb3956b71
Similar_Username <mannyjrmabalot@gmail.com>
2026-03-26 14:38:10 +0800

    Create base.sh

---
+++ scripts/base.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CORE_PACKAGES=(
+    git          
+    base-devel  
+    sudo        
+    curl        
+    wget
+    unzip
+    tar 
+    htop
+    nano
+    jq
+)
+
+install_pkg "${CORE_PACKAGES[@]}"
