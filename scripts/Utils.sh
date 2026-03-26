16eb8e9621d9d1b1975c7269b0f44ff7bfba4255
Similar_Username <mannyjrmabalot@gmail.com>
2026-03-26 17:05:58 +0800

    Update and rename network.sh to utils.sh

---
+++ scripts/Utils.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Installing  dependencies..."
+
+NETWORK_PACKAGES=(
+    NetworkManager
+    network-manager-applet
+    bluez
+    bluez-utils
+    bluez-libs
+    blueman
+)
+
+AUDIO_PACKAGES=(
+    pipewire
+    pipewire-pulse
+    wireplumber
+    pavucontrol
+    alsa-utils
+)
+
+install_pkg "${NETWORK_PACKAGES[@]}"
+install_pkg "${AUDIO_PACKAGES[@]}"
