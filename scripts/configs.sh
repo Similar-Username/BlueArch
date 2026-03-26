5c236902c0ca1a9bc8dafde47ead42454eff25d3
Similar_Username <mannyjrmabalot@gmail.com>
2026-03-26 17:30:09 +0800

    Update configs.sh

--- scripts/configs.sh
+++ scripts/configs.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+echo " Applying configurations..."
+
+FONTS_THEMES=(
+    ttf-jetbrains-mono-nerd   
+    noto-fonts-emoji         
+    papirus-icon-theme        
+)
+
+install_pkg "${FONTS_THEMES[@]}"
+
+cp -r ../configs/* ~/.config/
