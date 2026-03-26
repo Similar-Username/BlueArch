f3701ad37a0875971f9a66edd8f79c707a7e3049
Similar_Username <mannyjrmabalot@gmail.com>
2026-03-26 16:12:37 +0800

    Update install.sh
    Updated install_pkg with yay

--- install.sh
+++ install.sh
@@ -10,38 +10,64 @@ install_pacman() {
         return 0
     fi
 
-    echo "Installing $pkg..."
+    echo "Installing $pkg via pacman..."
     sudo pacman -S --needed --noconfirm "$pkg"
 }
 
 
-
-install_aur() {
-    local package="$1"
-    local tmpdir="/tmp/$package"
+install_yay(){
+    local pkg="$1"
     
-    if ! command -v "$package" &> /dev/null; then
-        echo " Installing $package from AUR..."
-
-        git clone "https://aur.archlinux.org/$package.git" "$tmpdir"
-        cd "$tmpdir" || { echo "Failed to enter $tmpdir"; return 1; }
-        makepkg -si --noconfirm
-
-        cd - || exit
-        rm -rf "$tmpdir"
-    else
-        echo "$package is already installed."
+    if pacman -Q "$pkg" &> /dev/null || yay -Q "$pkg" &> /dev/null; then
+        echo "$pkg already installed"
+        return 0
     fi
+    
+    echo "Installing $pkg via yay..."
+    yay -S --needed --noconfirm "$pkg" || return 1
 }
+
+
+install_clone() {
+    local pkg="$1"
+    local tmpdir="/tmp/$pkg"
+    
+    if command -v "$pkg" &> /dev/null; then
+        echo "$pkg is already installed."
+        return 0
+    if
+    
+    echo "Making $pkg via git clone..."
+    git clone "https://aur.archlinux.org/$pkg.git" "$tmpdir" || return 1
+    
+    (
+        cd "$tmpdir" || return 1
+        makepkg -si --noconfirm
+    )
+    
+    local result=$?
+    
+    rm -rf "$tmpdir"
+    return $result
+}
+
 
 
 install_pkg() {
     for pkg in "$@"; do
-        if ! install_pacman "$pkg"; then
-            if ! install_aur "$pkg"; then
-                echo "Failed to install $pkg."
-            fi
+        if install_pacman "$pkg"; then
+            continue
         fi
+        
+        if install_yay "$pkg"; then
+            continue
+        fi
+
+        if install_clone "$pkg"; then
+            continue
+        fi
+
+        echo "Failed to install $pkg."
     done
 }
 
