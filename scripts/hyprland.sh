fd1ea0e6184a3c888a29af078e0d8b17ae348402
Similar_Username <mannyjrmabalot@gmail.com>
2026-03-26 14:59:45 +0800

    Create hyprland.sh

---
+++ scripts/hyprland.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "➡️ Installing Hyprland and core Wayland dependencies..."
+
+HYPR_PACKAGES=(
+    hyprland
+    wlroots
+    wayland-protocols
+    xorg-xwayland
+    xdg-desktop-portal-hyprland
+    gtk3
+    gtk4
+    qt5-wayland
+    qt5ct
+    qt6-wayland
+    qt6ct
+)
+
+install_pkg "${HYPR_PACKAGES[@]}"
