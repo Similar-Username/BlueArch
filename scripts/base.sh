32fd216aeeefed023a0b95d685e40234f262cf4c
Nym <mannyjrmabalot@gmail.com>
2026-04-06 16:39:02 +0800

    Update base.sh
    Syntaxing, removed htop

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-echo "Installing Core dependencies..."
-
-CORE_PACKAGES=(
-    git          
-    base-devel  
-    sudo        
-    curl        
-    wget
-    unzip
-    tar 
-    htop
-    nano
-    jq
+# repo list: pkg|package_manager|description
+base_repos=(
+    "git         |pacman  |Version control"
+    "base-devel  |pacman  |Build tools"
+    "sudo        |pacman  |Privilege escalation"
+    "curl        |pacman  |HTTP client"
+    "wget        |pacman  |File downloader"
+    "unzip       |pacman  |ZIP extraction"
+    "tar         |pacman  |Archive tool"
+    "nano        |pacman  |Text editor"
+    "jq          |pacman  |JSON processor"
 )
 
-install_pkg "${CORE_PACKAGES[@]}"
+install_pkg "${base_repos[@]}"
